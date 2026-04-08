0d057313251287f2e4d4894ac99a53d067e267c3
JAEMO-LIM <tjdwkddl@gmail.com>
2026-04-08 22:26:41 +0900

    프로젝트 초기 설정

---
+++ src/main/java/net/zero123/crushspring/CrushSpringApplication.java
@@ -0,0 +1,13 @@
+package net.zero123.crushspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrushSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrushSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/zero123/crushspring/CrushSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zero123.crushspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrushSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
