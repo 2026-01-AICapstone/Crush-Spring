dbcd5daa0db458266deb316a08f558d5bc423fdd
JAEMO-LIM <tjdwkddl@gmail.com>
2026-04-14 15:42:40 +0900

    refactor: 비동기를 사용하기 위한 애너테이션 추가

--- src/main/java/net/zero123/crushspring/CrushSpringApplication.java
+++ src/main/java/net/zero123/crushspring/CrushSpringApplication.java
@@ -2,8 +2,10 @@ package net.zero123.crushspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CrushSpringApplication {
 
     public static void main(String[] args) {
